db82b6f61574038cf69352c591d36ae8335982fa
ArtemMayorov1 <kuzifidiracya@gmail.com>
2026-01-10 21:40:06 +0300

    Delete jest.config.cjs

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-	testEnvironment: "jsdom",
-	transform: {
-		"^.+\\.(t|j)sx?$": ["@swc/jest"],
-	},
-	modulePathIgnorePatterns: ["e2e"],
-};
